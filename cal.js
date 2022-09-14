de54bc3c1fbe1f8142bedaf52a4e98d0904c3c5a
Chizzy250 <104734604+Chizzy250@users.noreply.github.com>
2022-09-14 11:18:01 +0100

    Update cal.js

--- cal.js
+++ cal.js
@@ -1,3 +1,11 @@
 function zoom() {
   document.body.style.zoom = "105%";
 }
+
+let btn = document.querySelector("#toggleAll")
+let container = document.querySelector(".container-fluid")
+
+btn.addEventListener("click", function() {
+ 
+  container.classList.toggle("black")
+})
